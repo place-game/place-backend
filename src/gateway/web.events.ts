8c28a38b93d4923f009ca0e7994e45de0b8dee23
supat <supatachbeam@gmail.com>
2022-04-11 15:26:24 +0700

    refactor: removed unused variables

--- src/gateway/web.events.ts
+++ src/gateway/web.events.ts
@@ -1,18 +1,12 @@
 export enum Events {
-    CREATE = 'create',
-    ON_CREATE = 'on-create',
-    LEAVE = 'leave',
-    ON_LEAVE = 'on-leave',
-    JOIN = 'join',
-    ON_JOIN = 'on-join',
-    INTRODUCE = 'introduce',
-    GET_PLAYERS = 'get-players',
-    ON_GET_PLAYERS = 'on-get-players',
-  }
-  export interface Introduction {
-    username: string
-  }
-  
-  export interface ClientState {
-    username?: string
-  }
+  LEAVE = 'leave',
+  ON_LEAVE = 'on-leave',
+  JOIN = 'join',
+  ON_JOIN = 'on-join',
+  GET_PLAYERS = 'get-players',
+  ON_GET_PLAYERS = 'on-get-players',
+}
+
+export interface ClientState {
+  username?: string;
+}
