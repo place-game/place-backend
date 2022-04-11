e1e8389c4191dea30ca27ae0624b980fa68c7071
supat <supatachbeam@gmail.com>
2022-04-11 13:47:58 +0700

    feat: handle player count

--- src/gateway/web.gateway.ts
+++ src/gateway/web.gateway.ts
@@ -1,38 +1,53 @@
 import {
-    SubscribeMessage,
-    WebSocketGateway,
-    OnGatewayInit,
-    WebSocketServer,
-    OnGatewayConnection,
-    OnGatewayDisconnect,
-   } from '@nestjs/websockets';
-   import { Logger } from '@nestjs/common';
-   import { Socket, Server } from 'socket.io';
-   
-   @WebSocketGateway({
-     cors: {
-       origin: '*',
-     },
-   })
-   export class WebGateway implements OnGatewayInit, OnGatewayConnection, OnGatewayDisconnect {
-   
-    @WebSocketServer() server: Server;
-    private logger: Logger = new Logger('WebGateway');
-   
-    @SubscribeMessage('msgToServer')
-    handleMessage(client: Socket, payload: string): void {
-     this.server.emit('msgToClient', payload);
-    }
-   
-    afterInit(server: Server) {
-     this.logger.log('Init');
-    }
-   
-    handleDisconnect(client: Socket) {
-     this.logger.log(`Client disconnected: ${client.id}`);
-    }
-   
-    handleConnection(client: Socket, ...args: any[]) {
-     this.logger.log(`Client connected: ${client.id}`);
-    }
-   }
+  SubscribeMessage,
+  WebSocketGateway,
+  OnGatewayInit,
+  WebSocketServer,
+  OnGatewayConnection,
+  OnGatewayDisconnect,
+} from '@nestjs/websockets';
+import { Logger } from '@nestjs/common';
+import { Socket, Server } from 'socket.io';
+import { Events } from './web.events';
+
+var userCount = -1; //minus admin
+@WebSocketGateway({
+  cors: {
+    origin: '*',
+  },
+})
+export class WebGateway
+  implements OnGatewayInit, OnGatewayConnection, OnGatewayDisconnect
+{
+  @WebSocketServer() server: Server;
+  private logger: Logger = new Logger('WebGateway');
+
+  @SubscribeMessage('msgToServer')
+  handleMessage(client: Socket, payload: string): void {
+    this.server.emit('msgToClient', payload);
+  }
+
+  @SubscribeMessage(Events.ON_GET_PLAYERS)
+  async getConcurrentPlayers(server = this.server) {
+    await server?.emit(Events.GET_PLAYERS, { current: userCount ?? 0 });
+  }
+
+  afterInit(server: Server) {
+    this.logger.log('Init');
+  }
+
+  async handleDisconnect(client: Socket) {
+    this.logger.log(`Client disconnected: ${client.id}`);
+    userCount--;
+    await this.server?.emit(Events.GET_PLAYERS, { current: userCount ?? 0 });
+
+  }
+
+  async handleConnection(client: Socket, ...args: any[]) {
+    this.logger.log(`Client connected: ${client.id}`);
+    userCount++;
+    await this.server?.emit(Events.GET_PLAYERS, { current: userCount ?? 0 });
+
+
+  }
+}
